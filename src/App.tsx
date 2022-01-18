6238cf0cfaf9e4fb42035eff85263a3d43029ecc
oLemos <lemmos@outlook.com.br>
2022-01-18 20:12:43 -0300

    init application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
+import { Header } from "./components/Header";
+
+export function App() {
+    return (
+        <>
+            <Header />
+            <GlobalStyle />
+        </>
+    );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --background: #f0f2f5;
+        --shape: #ffffff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    /*
+        Utilizaremos a medida "rem" no lugar de "px". Pois a medida "rem", leva em conta o tamanho da fonte padrão do projeto.
+        Agora, aqui no arquivo global iremos utilizar porcentagem, pois caso o usuário esteja utilizando opções de acessibilidade diferentes do padrão,
+        como deixar os elementos maiores ou menores, a porcentagem seguirá esta opção do usuário.
+    */
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%; // 15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    // Estes elementos por padrão não pegam a fonte da tag "body". Por isso estamos passando para todos eles aqui.
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
