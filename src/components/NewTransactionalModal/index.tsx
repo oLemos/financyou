03086a2e49ab61cbd712f449f020690365757c09
Lemos <lemmos@outlook.com.br>
2022-01-23 17:29:00 -0300

    modals and forms

--- src/components/NewTransactionalModal/index.tsx
+++ src/components/NewTransactionalModal/index.tsx
@@ -1,8 +1,13 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
-import { Container } from './styles';
+import { api } from '../../services/api';
+
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 Modal.setAppElement('#root');
 
@@ -12,6 +17,25 @@ interface NewTransactionalModalProps {
 }
 
 export const NewTransactionalModal = ({ isOpen, onRequestClose }: NewTransactionalModalProps) => {
+    const [value, setValue] = useState(0);
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [type, setType] = useState('deposit');
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        // Para previnir o recarregamento descenssário da página.
+        event.preventDefault();
+
+        const data = {
+            value,
+            title,
+            category,
+            type
+        }
+
+        api.post('/transactions', data)
+    }
+
     return (
         <Modal
             isOpen={isOpen}
@@ -27,12 +51,49 @@ export const NewTransactionalModal = ({ isOpen, onRequestClose }: NewTransaction
                 <img src={closeImg} alt="Fechar modal" />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
-                <input type="text" placeholder="Título" />
-                <input type="number" placeholder="Valor" />
-                <input type="text" placeholder="Categoria" />
+                <input
+                    type="text"
+                    placeholder="Título"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <input
+                    type="number"
+                    placeholder="Valor"
+                    value={value}
+                    onChange={e => setValue(Number(e.target.value))}
+                />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType('deposit')}
+                        isActive={type === 'deposit'}
+                        activeColor="green"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        isActive={type === 'withdraw'}
+                        onClick={() => setType('withdraw')}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
+                <input
+                    type="text"
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}
+                />
                 <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
--- src/components/TranscationsTable/index.tsx
+++ src/components/TranscationsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { api } from "../../services/api"
 import { Container } from "./styles"
 
+interface TransactionProps {
+    id: number;
+    title: string;
+    type: string;
+    category: string;
+    amount: number;
+    createdAt: string;
+}
+
 export const TransactionsTable = () => {
+    const [transactions, setTransactions] = useState<TransactionProps[]>([]);
+
     useEffect(() => {
         api.get('/transactions')
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return (
@@ -21,19 +32,21 @@ export const TransactionsTable = () => {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de sites</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>18/01/2022</td>
-                    </tr>
-
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.300</td>
-                        <td>Casa</td>
-                        <td>16/01/2022</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
